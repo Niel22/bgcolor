a417e9a13075fd034ca0cae91c3f9599962aac2a
Niel22 <125226257+Niel22@users.noreply.github.com>
2023-10-25 11:46:36 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,2 @@
+var bgcolor = new Array("#029fda", "#d43702", "#1eff00", "#eaeaff", "#ffea00", "#ab5252", "#ffffff", "#de28d8");
+document.body.style.background = bgcolor[Math.floor(Math.random() * bgcolor.length)];
